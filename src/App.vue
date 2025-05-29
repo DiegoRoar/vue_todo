28fbf009473b726a642dfbb15624ce91d70cc25d
Diego Hernandez <dalvhst@gmail.com>
2025-05-29 17:47:34 -0500

    css start reset
    Reset CSS for app.vue, and main.css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div>Ela</div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 2rem 1rem;
+  box-sizing: border-box;
+}
+</style>
